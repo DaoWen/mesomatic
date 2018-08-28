df2c2ca5fe93c59d47161f2e008aae33e1e7893c
Nick Vrvilo <Nick.Vrvilo@alumni.rice.edu>
2018-08-28 10:26:52 -0500

    Release twosigma/mesomatic 1.0.1-2018.08.28

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/mesomatic "1.0.1-r0"
+(defproject twosigma/mesomatic "1.0.1-2018.08.28"
   :description "Apache Mesos type conversions to and from Clojure"
   :url "https://github.com/pyr/mesomatic"
   :license {:name "MIT License"}
